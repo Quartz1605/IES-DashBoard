649cc0eb2d5a64bf0438a03b8dbf1416bbf8fb67
Quartz1605 <prathambarekal05@gmail.com>
2025-08-20 18:56:24 +0530

    Done with frontend

---
+++ IESDashboard/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex items-center h-screen justify-center text-amber-400'>Hello pratham</div>
+    </>
+  )
+}
+
+export default App
